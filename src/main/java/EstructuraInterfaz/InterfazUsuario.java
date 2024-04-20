cb10cfaf44321dc647dc5b4fc22fab7086aa812c
fferrlop <fferrlop@myuax.com>
2024-04-20 10:41:22 +0200

    Se abre una ventana

---
+++ src/main/java/EstructuraInterfaz/InterfazUsuario.java
@@ -0,0 +1,14 @@
+package EstructuraInterfaz;
+
+import javax.swing.JFrame;
+
+public class InterfazUsuario extends JFrame {
+
+    public InterfazUsuario() {
+        setTitle("Inicio");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
