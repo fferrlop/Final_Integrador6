e1134265aa79da5fa46c9d3563abdae4286a5db2
fferrlop <fferrlop@myuax.com>
2024-04-22 17:50:33 +0200

    botón para analisis multidimensional

--- src/main/java/DatosDinámicos/EsteticaDatos.java
+++ src/main/java/DatosDinámicos/EsteticaDatos.java
@@ -2,6 +2,8 @@ package DatosDinámicos;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class EsteticaDatos extends JFrame {
     public EsteticaDatos() {
@@ -17,5 +19,23 @@ public class EsteticaDatos extends JFrame {
         JLabel label = new JLabel("Datos dinámicos", SwingConstants.CENTER);
         label.setForeground(Color.WHITE);
         getContentPane().add(label, BorderLayout.NORTH);
+
+        // Crear un botón
+        JButton button = new JButton("Modelado Multidimensional");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Llamar al método main de ModeladoMultidimensional
+                ModeladoMultidimensional.main(new String[]{});
+            }
+        });
+
+        // Crear un JPanel con FlowLayout y agregar el botón a este
+        JPanel panel = new JPanel(new FlowLayout());
+        panel.setBackground(customColor); // Establecer el color de fondo del JPanel
+        panel.add(button);
+
+        // Agregar el JPanel a la ventana
+        getContentPane().add(panel, BorderLayout.CENTER);
     }
 }
